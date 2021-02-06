8ed696c28f55b7ce2a5598688ba85c83d2bc2c19
bryan <bryanjm96@gmail.com>
2021-02-06 16:08:40 -0600

    testeos de funcion

--- aps.js
+++ aps.js
@@ -5,6 +5,12 @@ myForm.addEventListener('submit', async function (e) {
     e.preventDefault();
 const objeto = campos(e);
 console.log(objeto);
+
+if(objeto.status ==="error"){
+    return alert("hay campos vacios");
+}
+
+
 const data = await fetchData("http://localhost:3000/maestro", 'POST', objeto);
 console.log(data);
 })
